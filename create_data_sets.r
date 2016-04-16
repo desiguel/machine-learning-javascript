871e73725da8a6a4df13c7940fad40b7dbb11797
desiguel <desiguel@gmail.com>
2016-04-16 16:20:38 +1000

    Data generation script.

---
+++ create_data_sets.r
@@ -0,0 +1,38 @@
+jitterpoints = 40
+jittersd = 0.40
+
+# Function to generate random points.
+p = function(point){
+  w = matrix(cbind(rnorm(n = jitterpoints, mean = as.numeric(point[1]), sd = jittersd), 
+                   rnorm(n = jitterpoints, mean = as.numeric(point[2]), sd = jittersd),
+                   point[3]), ncol=3)
+  return(w)
+}
+
+# Points required.
+points = list(c(1,1,"apples"),
+              c(1,2,"oranges"),
+              c(1,3,"pears"),
+              c(2,1,"bananas"),
+              c(2,2,"peaches"),
+              c(2,3,"watermelon"),
+              c(3,1,"rockmelon"),
+              c(3,2,"rasberries"),
+              c(3,3,"mandarins"),
+              c(4,1,"blueberries"),
+              c(4,2,"nectarines"),
+              c(4,4,"grapes"))
+
+# Generate the data.
+df = lapply(points, p)
+df = data.frame(do.call("rbind",df))
+
+# Relabel
+colnames(df) = c("x","y","class")
+
+# Cut down size of decimals
+df$x = round(as.numeric(as.character(df$x)), 3)
+df$y = round(as.numeric(as.character(df$y)), 3)
+
+plot(df$x, df$y, col=df$class)
+legend(2,2,unique(df$class),col=1:length(df$class),pch=1)
--- experiences.md
+++ experiences.md
@@ -113,3 +113,8 @@ Implementation of webworker-threads (multi-core node processing)
 # Conclusions
 
 Node is usable for basic ML activities. For anything custom you'll have to write your own library.
+
+
+# Examples
+
+http://playground.tensorflow.org/#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.03&regularizationRate=0&noise=0&networkShape=4,2&seed=0.31981&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification
